ebf3b871782cfa18bff0108712a3cb38e3808d56
François Michonneau <francois.michonneau@gmail.com>
2020-04-10 17:36:30 -0400

    label messages in Gmail with success/failure

--- R/caching.R
+++ R/caching.R
@@ -16,12 +16,56 @@ get_gmail_thread <- function(thread_id, namespace) {
 }
 
 
+### Gmail labels ---------------------------------------------------------------
+
+fetch_hook_gmail_labels <- function(key, namespace) {
+  all_labels <- gmailr::gm_labels()
+  all_label_names <- purrr::map_chr(all_labels$labels, ~ .$name)
+
+  m <- match(key, all_label_names)
+
+  if (is.na(m)) {
+    message("Creating label: ", key)
+    new_label <- gmailr::gm_create_label(key)
+    new_label$id
+  } else {
+    all_labels$labels[[m]]$id
+  }
+}
+
+store_gmail_labels <- function(path = "cache/gmail-labels") {
+  storr::storr_external(
+    storr::driver_rds(path),
+    fetch_hook_gmail_labels
+  )
+}
+
+get_gmail_label <- function(label) {
+  store_gmail_labels()$get(label)
+}
+
 
 ### HelpSout queries responses -------------------------------------------------
 
 fetch_hook_hs_response <- function(key, namespace) {
   res <- get_gmail_thread(key, namespace)
-  hs_create_thread(res$get(), htoken)
+  rsp <- hs_create_thread(res$get(), htoken)
+
+  if (identical(httr::content(rsp, "text", encoding = "utf-8"), "")) {
+    lbl <- paste0("success-", namespace)
+    my_gm_modify_thread(
+      id = key, add_labels = get_gmail_label(lbl)
+    )
+  } else {
+    message("failure for thread: ", key)
+    message(httr::content(rsp, "text", encoding = "utf-8"))
+    lbl <- paste0("failure-", namespace)
+    my_gm_modify_thread(
+      id = key, add_labels = get_gmail_label(lbl)
+    )
+  }
+
+  rsp
 }
 
 store_hs_responses <- function(path = "cache/hs_responses") {
